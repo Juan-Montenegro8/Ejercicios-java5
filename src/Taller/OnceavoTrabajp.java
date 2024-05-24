be5a0be93c638fbdbb6cf16c3fbd1418a08c2291
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-05-23 19:50:55 -0500

    fifth commit of taller

--- src/Taller/OnceavoTrabajp.java
+++ src/Taller/OnceavoTrabajp.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class OnceavoTrabajp {
 
     public static void main(String[] args) {
-        Scanner t = new Scanner(System.in);
+        Scanner Texto = new Scanner(System.in);
         
         System.out.println("ingrese numero");
-        int n=t.nextInt();
-        int arr []= new int[n];
+        int numero=Texto.nextInt();
+        int array []= new int[numero];
         
         System.out.println("llenar arreglo");
         
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] =t.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            array[i] =Texto.nextInt();
         }
         
-        System.out.println("suma: "+sum(arr,0));
+        System.out.println("suma: "+Suma(array,0));
     }
-    public static int sum(int[] arr, int i){
+    public static int Suma(int[] array, int i){
         int suma=0;
-        if (i>=arr.length) {
+        if (i>=array.length) {
             return suma;
         }else{
-            suma = arr[i];
-            return suma + sum(arr, i+1);
+            suma = array[i];
+            return suma + Suma(array, i+1);
         }
     }
 }
