de12c7f58b9c7cffe93374367b80cf9e26045c61
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-26 14:09:03 -0500

    first commit scanner

--- src/Taller/PrimerTrabajo.java
+++ src/Taller/PrimerTrabajo.java
@@ -6,10 +6,13 @@ public class PrimerTrabajo {
 
     public static void main(String[] args) {
         Scanner texto = new Scanner(System.in);
-        
-        System.out.println("ingrese limite");
-        long numero=texto.nextLong();
-        System.out.println("el factorial de "+numero+" es: "+factorial(numero));
+        try {
+            System.out.println("ingrese limite");
+            long numero=texto.nextLong();
+            System.out.println("el factorial de "+numero+" es: "+factorial(numero));
+        } catch (Exception e) {
+            texto.close();
+        }
     }
     static long factorial(long number){
         if(number==0){
