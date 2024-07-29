dda1dd6bc95fce26afde10bbd0e77d080994aa14
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-29 14:44:42 -0500

    second commit scanner

--- src/Taller/SegundoTrabajo.java
+++ src/Taller/SegundoTrabajo.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class SegundoTrabajo {
 
     public static void main(String[] args) {
-        Scanner texto = new Scanner(System.in);
+        Scanner textoScanner = new Scanner(System.in);
         
         System.out.println("ingrese limite");
-        long numero=texto.nextLong();
+        long numero=textoScanner.nextLong();
         System.out.println("el sumatoria de "+numero+" es: "+sumatoria(numero));
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
     static long sumatoria(long number){
         if(number==0){
