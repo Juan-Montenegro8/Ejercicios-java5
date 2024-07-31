1d26d46432b55af12d6cf942b08ddf881440a319
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-07-30 20:15:23 -0500

    Seconds commit scanner

--- src/Taller/CuartoTrabajo.java
+++ src/Taller/CuartoTrabajo.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class CuartoTrabajo {
 
     public static void main(String[] args) {
-        Scanner texto = new Scanner(System.in);
+         
+        try (Scanner textoScanner = new Scanner(System.in)){
+            System.out.println("ingrese numero");
+            int numero=textoScanner.nextInt();
         
-        System.out.println("ingrese numero");
-        int numero=texto.nextInt();
-        
-        System.out.println("EL invertido es: "+invertido(numero));        
+            System.out.println("EL invertido es: "+invertido(numero));  
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     public static String invertido(int number){
         if (10>number) {
