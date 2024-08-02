0b461741d8bce8c27ff63c3b8f7a5aea761ed429
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-01 22:25:02 -0500

    Third commit scanner

--- src/Taller/SextoTrabajo.java
+++ src/Taller/SextoTrabajo.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class SextoTrabajo {
 
     public static void main(String[] args) {
-        Scanner textoScanner = new Scanner(System.in);
+        try (Scanner textoScanner = new Scanner(System.in)) {
+            System.out.println("ingrese numero");
+            int numero=textoScanner.nextInt();
         
-        System.out.println("ingrese numero");
-        int numero=textoScanner.nextInt();
-        
-        System.out.println("Sumatoria: "+Sumatoria(numero));
+            System.out.println("Sumatoria: "+Sumatoria(numero));
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
     public static int Sumatoria(int number){
