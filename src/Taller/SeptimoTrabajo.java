799ab79fb1b23b3c0a9c774243de46632bdae55f
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-01 22:26:20 -0500

    Thirds commit scanner

--- src/Taller/SeptimoTrabajo.java
+++ src/Taller/SeptimoTrabajo.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class SeptimoTrabajo {
 
     public static void main(String[] args) {
-        Scanner TextoScanner = new Scanner(System.in);
         
-        System.out.println("ingrese numero");
-        int numero=TextoScanner.nextInt();
-        System.out.println("exponencial");
-        int exponencial=TextoScanner.nextInt();
+        try (Scanner TextoScanner = new Scanner(System.in)) {
+            System.out.println("ingrese numero");
+            int numero=TextoScanner.nextInt();
+            System.out.println("exponencial");
+            int exponencial=TextoScanner.nextInt();
         
-        System.out.println("resultado es: "+Exponencial(numero,exponencial));
+            System.out.println("resultado es: "+Exponencial(numero,exponencial));
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     public static int Exponencial(int number, int exponen){
         if (exponen==0) {
