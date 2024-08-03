80484d3b3020a5774688e5795851901e3c5b4e33
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-02 22:19:07 -0500

    fouths commit scanner

--- src/Taller/NovenoTrabajo.java
+++ src/Taller/NovenoTrabajo.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class NovenoTrabajo {
 
     public static void main(String[] args) {
-        Scanner TextoScanner = new Scanner(System.in);
+        try (Scanner TextoScanner = new Scanner(System.in)){
+            
+            System.out.println("ingrese numero");
+            int numero=TextoScanner.nextInt();
+            System.out.println("ingrese numero");
+            int Numero=TextoScanner.nextInt();
         
-        System.out.println("ingrese numero");
-        int numero=TextoScanner.nextInt();
-        System.out.println("ingrese numero");
-        int Numero=TextoScanner.nextInt();
-        
-        System.out.println("Coociente: "+Coociente(numero,Numero));
+            System.out.println("Coociente: "+Coociente(numero,Numero));
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     public static int Coociente(int number, int Number) {
         if (number > Number)
