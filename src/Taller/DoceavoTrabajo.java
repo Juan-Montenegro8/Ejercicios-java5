d66ca1e250267d397eeee1468d17a26b360c1787
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-07 19:57:38 -0500

    Fifths commit scanner

--- src/Taller/DoceavoTrabajo.java
+++ src/Taller/DoceavoTrabajo.java
@@ -5,32 +5,36 @@ import java.util.Scanner;
 public class DoceavoTrabajo {
 
     public static void main(String[] args) {
-        Scanner TextoScanner = new Scanner(System.in);
         
-        System.out.println("ingrese numero");
-        int numero=TextoScanner.nextInt();
-        System.out.println("ingrese numero");
-        int Numero=TextoScanner.nextInt();
+        try (Scanner TextoScanner = new Scanner(System.in)){
+
+            System.out.println("ingrese numero");
+            int numero=TextoScanner.nextInt();
+            System.out.println("ingrese numero");
+            int Numero=TextoScanner.nextInt();
         
-        int array[][]=new int[numero][Numero];
+            int array[][]=new int[numero][Numero];
         
-        System.out.println("llenar arreglo");
+            System.out.println("llenar arreglo");
         
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j]=TextoScanner.nextInt();
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j]=TextoScanner.nextInt();
+                }
             }
-        }
         
-        System.out.println("mostrar arreglo");
+            System.out.println("mostrar arreglo");
         
-        for (int i = 0; i < numero; i++) {
-            for (int j = 0; j < Numero; j++) {
-                System.out.println(array[i][j]+" ");
+            for (int i = 0; i < numero; i++) {
+                for (int j = 0; j < Numero; j++) {
+                    System.out.println(array[i][j]+" ");
+                }
             }
-        }
         
-        System.out.println("multiplicacion: "+Multiplicacion(array,numero-1,Numero-1));
+            System.out.println("multiplicacion: "+Multiplicacion(array,numero-1,Numero-1));    
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     public static int Multiplicacion(int[][] array, int number, int Number){
         if ((number == 0) && (Number == 0)) {
