a51535acd89bbe20bbbc383f172124b6de49bb7d
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-13 20:38:56 -0500

    firt commit scanner

--- src/cuarto/trabajo/CuartoTrabajo.java
+++ src/cuarto/trabajo/CuartoTrabajo.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class CuartoTrabajo {
 
     public static void main(String[] args) {
-        Scanner texto = new Scanner(System.in);
+        Scanner textoScanner = new Scanner(System.in);
         
         System.out.println("ingrese numero");
-        int numero=texto.nextInt();
+        int numero=textoScanner.nextInt();
         
         recursivo(numero);
         System.out.println("regreso main");
