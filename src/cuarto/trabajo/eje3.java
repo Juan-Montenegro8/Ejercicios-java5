34b0bce93213b25f2fac0345685b47719646b7d6
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-14 20:38:34 -0500

    Firts commit scanner

--- src/cuarto/trabajo/eje3.java
+++ src/cuarto/trabajo/eje3.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class eje3 {
 
     public static void main(String[] args) {
-        Scanner textoScanner = new Scanner(System.in);
         
-        System.out.println("ingrese numero");
-        int numero=textoScanner.nextInt();
+        try (Scanner textoScanner = new Scanner(System.in)) {
+            System.out.println("ingrese numero");
+            int numero=textoScanner.nextInt();
         
-        recursivo(numero);
-        System.out.println("regreso main");
+            recursivo(numero);
+            System.out.println("regreso main");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     static void recursivo(int number){
         if(1<number)
