eee28b75ba8de9a24f17ff87c0c944a237a7ae65
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-08-16 20:38:33 -0500

    Seconds commit scanner

--- src/cuarto/trabajo/ejemplo2.java
+++ src/cuarto/trabajo/ejemplo2.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class ejemplo2 {
 
     public static void main(String[] args) {
-        Scanner textoScanner = new Scanner(System.in);
+        try (Scanner textoScanner = new Scanner(System.in)) {
+            System.out.println("ingrese numero");
+            int numero=textoScanner.nextInt();
         
-        System.out.println("ingrese numero");
-        int numero=textoScanner.nextInt();
-        
-        recursivo(numero,1);
-        System.out.println("regreso main");
+            recursivo(numero,1);
+            System.out.println("regreso main");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     static void recursivo(int number,int i){
         if(i<=number){
